bbf70b6479f4facd59772a415d3593d4c24e839d
Sachin-Gola <sachingola343@gmail.com>
2025-02-04 14:25:01 +0530

    Initial Commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const router=express.Router();
+
+router.get('/login',(req,res)=>{
+    res.render('users/login.ejs');
+})
+router.get('/signup',(req,res)=>{
+    res.render('users/signup.ejs');
+})
+module.exports=router;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express =require ('express');
+const ejsmate=require('ejs-mate');
+const userRoutes=require('./routes/userRoutes.js')
+const productRoutes=require('./routes/productRoutes.js')
+
+const app=express();
+
+
+app.engine('ejs',ejsmate)
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.use('/u1',userRoutes);
+app.use('/p1',productRoutes);
+
+ app.listen(9000,()=>{
+    console.log("Server is running at port 9000");
+    
+ })
+
